cf7aaae459203ce32e32bac15924547be57b96a7
K\K <kwonabraham@naver.com>
2024-03-11 23:55:29 +0900

    save(회원등록) 메소드 작성

--- src/main/java/com/study/erum/controller/BoardController.java
+++ src/main/java/com/study/erum/controller/BoardController.java
@@ -1,16 +1,36 @@
 package com.study.erum.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.study.erum.dto.BoardDTO;
+import com.study.erum.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+	
+	private final BoardService boardService;
+	
     @GetMapping("/save")
     public String saveForm(){
         return "save";
     }
+    
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        int saveResult = boardService.save(boardDTO);
+        if (saveResult > 0) {
+            return "redirect:/board/";
+        } else {
+            return "save";
+        }
+    }
+
 }
