0612473ef5cd2fd002e74f38ccdc534bf2058791
K\K <kwonabraham@naver.com>
2024-03-12 00:33:32 +0900

    MyBatis 설정 완료

--- src/main/java/com/study/erum/repository/BoardRepository.java
+++ src/main/java/com/study/erum/repository/BoardRepository.java
@@ -1,5 +1,18 @@
 package com.study.erum.repository;
 
-public class BoardRepository {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
+import com.study.erum.dto.BoardDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+    private final SqlSessionTemplate sql;
+
+    public int save(BoardDTO boardDTO) {
+        return sql.insert("Board.save", boardDTO);
+    }
 }
